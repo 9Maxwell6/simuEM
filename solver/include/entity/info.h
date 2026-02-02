b0ee5f1c08003f7661283a0ca9e2c9a6cbfb43b4
9Maxwell6 <zzy.zhenyan@gmail.com>
2026-02-02 07:38:15 +0100

    add element framework

---
+++ solver/include/entity/info.h
@@ -0,0 +1,24 @@
+#include <bitset>
+#include <stddef.h>
+#include <map>
+
+
+
+struct Element_Info {
+    std::string name;
+    std::vector<double> scalar_data;
+    std::vector<double> vector_data_x; 
+    std::vector<double> vector_data_y; 
+    std::vector<double> vector_data_z; 
+};
+
+
+
+class Info_handler
+{
+private:
+    
+
+
+    //
+};
---
+++ solver/src/entity/tetrahedron.cpp
@@ -0,0 +1,42 @@
+#include "entity/tetrahedron.h"
+
+Type Tetrahedron::get_Type(){
+    return Type::TETRAHEDRON;
+}
+
+Tetrahedron::Tetrahedron(const size_t *idx, size_t property_id)
+{
+   property_id_ = property_id;
+   for (int i = 0; i < 4; i++)
+   {
+      node_idx_[i] = idx[i];
+   }
+}
+
+Tetrahedron::Tetrahedron(size_t idx_1, size_t idx_2, size_t idx_3, size_t idx_4, size_t property_id)
+{
+    property_id_  = property_id;
+    node_idx_[0] = idx_1;
+    node_idx_[1] = idx_2;
+    node_idx_[2] = idx_3;
+    node_idx_[3] = idx_4;
+}
+
+void Tetrahedron::set_NodeIdx(const size_t *idx)
+{
+   for (int i = 0; i < 4; i++)
+   {
+      node_idx_[i] = idx[i];
+   }
+}
+
+void Tetrahedron::set_NodeIdx(size_t idx_1, size_t idx_2, size_t idx_3, size_t idx_4)
+{
+    node_idx_[0] = idx_1;
+    node_idx_[1] = idx_2;
+    node_idx_[2] = idx_3;
+    node_idx_[3] = idx_4;
+}
+
+
+
