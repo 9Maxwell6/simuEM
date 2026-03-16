3c9c9d3af0569f37960bfd2fb0ca97d8bdff12de
9Maxwell6 <1315764271@qq.com>
2026-03-16 17:54:54 +0100

    integrator framework

--- solver/include/math/fem/integrator.h
+++ solver/include/math/fem/integrator.h
@@ -3,12 +3,72 @@
 namespace simu {
 
 
-
+/**
+ * s: scalar coefficient
+ * v: vector coefficient
+ * S: scalar function
+ * V: vector function
+ */
 class Integrator
 {
 
+
     
-    
+};
+
+
+
+/**
+ * domain: H_1
+ * range:  H_1 
+ * 
+ */
+class Integrator__s_S__S : public Integrator
+{
+
+};
+
+
+/**
+ * domain: H_1
+ * range:  H_1 
+ * 
+ */
+class Integrator__s_grad_S__grad_S : public Integrator
+{
+
+};
+
+
+/**
+ * domain: H_1
+ * range:  H_1 
+ * 
+ */
+class Integrator__s_V__grad_S : public Integrator
+{
+
+};
+
+
+/**
+ * domain: H_curl
+ * range:  H_curl
+ * 
+ */
+class Integrator__s_curl_V__curl_V : public Integrator
+{
+
+};
+
+/**
+ * domain: H_curl, H_div
+ * range:  H_curl, H_div
+ * 
+ */
+class Integrator__s_V__V : public Integrator
+{
+
 };
 
 
